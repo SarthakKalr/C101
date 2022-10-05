7edfba244a9cb567dedc0bd8dc87c9ecd1b03089
SarthakKalr <104204863+SarthakKalr@users.noreply.github.com>
2022-10-05 18:11:16 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBuc891mUMxIoM0TQiUlqodahpPrSjr2cI",
+    authDomain: "letschatwebapp-c0796.firebaseapp.com",
+    databaseURL: "https://letschatwebapp-c0796-default-rtdb.firebaseio.com",
+    projectId: "letschatwebapp-c0796",
+    storageBucket: "letschatwebapp-c0796.appspot.com",
+    messagingSenderId: "492455617859",
+    appId: "1:492455617859:web:ceb8066c16b811689cbbf6"
+  };
+
+firebase.initializeApp(firebaseConfig);
